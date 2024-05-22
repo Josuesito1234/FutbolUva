0586fde0aeaf7b7ffce3eb76ccaed7bcd1b9bf3e
Albertito21 <815248@clases.edu.sv>
2024-05-22 15:23:27 -0600

    Otro cambio de Albertito

--- src/App.java
+++ src/App.java
@@ -45,6 +45,7 @@ public class App {
 
         System.out.println("Este es el cambioo >:)");
         System.out.println("Este es el cambio de Esaú osea mio");
+        System.out.println("Y este es el otro mio");
 
         System.out.println("Este es el cambio de natita:3");
         System.out.println("El barcita por siempre no madrilistas");
